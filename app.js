a0bc7de2f2d7e9e7e27a57ddd12fd4ac686f469e
Beknazar19 <45395471+Beknazar19@users.noreply.github.com>
2023-01-07 19:05:32 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,101 +1,47 @@
-let tg = window.Telegram.WebApp;
-
-tg.expand();
-
-tg.MainButton.textColor = "#FFFFFF";
-tg.MainButton.color = "#2cab37";
-
-let item = "";
-
-let btn1 = document.getElementById("btn1");
-let btn2 = document.getElementById("btn2");
-let btn3 = document.getElementById("btn3");
-let btn4 = document.getElementById("btn4");
-let btn5 = document.getElementById("btn5");
-let btn6 = document.getElementById("btn6");
-
-btn1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 1!");
-		item = "1";
-		tg.MainButton.show();
-	}
-});
-
-btn2.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 2!");
-		item = "2";
-		tg.MainButton.show();
-	}
-});
-
-btn3.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 3!");
-		item = "3";
-		tg.MainButton.show();
-	}
-});
-
-btn4.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 4!");
-		item = "4";
-		tg.MainButton.show();
-	}
-});
-
-btn5.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 5!");
-		item = "5";
-		tg.MainButton.show();
-	}
-});
-
-btn6.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 6!");
-		item = "6";
-		tg.MainButton.show();
-	}
-});
 
 
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	tg.sendData(item);
-});
+        var val_ask = document.getElementById("select_group_1");
+        var val_bid = document.getElementById("select_group_4");
+        
+        function onChange() {
+            var val_ask_num = val_ask.value;
+            var val_ask_text = val_ask.options[val_ask.selectedIndex].text;
 
-let usercard = document.getElementById("usercard");
-
-let p = document.createElement("p");
-
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
+            var val_bid_num = val_bid.value;
+            var val_bid_text = val_bid.options[val_bid.selectedIndex].text;
+            
 
 
-usercard.appendChild(p); 
+            let searchValue = Math.min(val_ask_num, val_bid_num);
+            let selectElement = document.getElementById('select_group_1');
+            let options = selectElement.options;
+
+            for (let i = 0; i < options.length; i++) {
+                if (options[i].value == searchValue) {
+                console.log(options[i].text, document.getElementById('summary').innerHTML);
+
+                document.getElementById('Label_Sum').innerHTML = 'Объем, в ' + options[i].text.substring(0, 3);
+                // + options[i].text;
+
+                break;
+                } }
+
+        }
+        val_ask.onchange = onChange;
+        val_bid.onchange = onChange;
+        
+        onChange();
 
 
 
 
+        //задаем формат для поле объем, чтобы только цифры были
+        document.getElementById('summary').addEventListener('input', function() {
+            this.value = this.value.replace(/[^\d]/g, '').replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ');
+        });
 
+        //задаем формат для поле курс, чтобы только цифры были
+        document.getElementById('rate').addEventListener('input', function() {
+            this.value = this.value.replace(/[^0-9.]/g, '', '')
+            // .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$0 ');
+        });
