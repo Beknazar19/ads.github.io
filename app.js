002d80baa851074334bb4e704886ec6616d12868
Beknazar19 <45395471+Beknazar19@users.noreply.github.com>
2023-01-14 00:39:20 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,52 +1,62 @@
 
+
+        var val_ask = document.getElementById("select_group_1");
+        var val_bid = document.getElementById("select_group_4");
+        
+        function onChange() {
+            var val_ask_num = val_ask.value;
+            var val_ask_text = val_ask.options[val_ask.selectedIndex].text;
+
+            var val_bid_num = val_bid.value;
+            var val_bid_text = val_bid.options[val_bid.selectedIndex].text;
+            
+
+
+            let searchValue = Math.min(val_ask_num, val_bid_num);
+            let selectElement = document.getElementById('select_group_1');
+            let options = selectElement.options;
+
+            for (let i = 0; i < options.length; i++) {
+                if (options[i].value == searchValue) {
+                console.log(options[i].text, document.getElementById('summary').innerHTML);
+
+                document.getElementById('Label_Sum').innerHTML = 'Объем, в ' + options[i].text.substring(0, 3);
+                // + options[i].text;
+
+                break;
+                } }
+
+        }
+        val_ask.onchange = onChange;
+        val_bid.onchange = onChange;
+        
+        onChange();
+
+
+
+
+        //задаем формат для поле объем, чтобы только цифры были
+        document.getElementById('summary').addEventListener('input', function() {
+            this.value = this.value.replace(/[^\d]/g, '').replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ');
+        });
+
+        //задаем формат для поле курс, чтобы только цифры были
+        document.getElementById('kurs').addEventListener('input', function() {
+            this.value = this.value.replace(/[^0-9.]/g, '', '')
+            // .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$0 ');
+        });
+
+
+
+
+
+
+
+
+
+
 let tg = window.Telegram.WebApp;
 tg.expand(); //расширяем на все окно  
-
-
-var val_ask_obj = document.getElementById("select_group_1");
-var val_bid_obj = document.getElementById("select_group_4");
-
-function onChange() {
-    var val_ask_num = val_ask_obj.value;
-    var val_ask_text = val_ask_obj.options[val_ask.selectedIndex].text;
-
-    var val_bid_num = val_bid_obj.value;
-    var val_bid_text = val_bid_obj.options[val_bid.selectedIndex].text;
-    
-
-
-    let searchValue = Math.min(val_ask_num, val_bid_num);
-    let selectElement = document.getElementById('select_group_1');
-    let options = selectElement.options;
-
-    for (let i = 0; i < options.length; i++) {
-        if (options[i].value == searchValue) {
-        console.log(options[i].text, document.getElementById('summary').innerHTML);
-
-        document.getElementById('Label_Sum').innerHTML = 'Объем, в ' + options[i].text.substring(0, 3);
-        // + options[i].text;
-
-        break;
-        } }
-}
-val_ask_obj.onchange = onChange;
-val_bid_obj.onchange = onChange;
-
-onChange();
-
-//задаем формат для поле объем, чтобы только цифры были
-document.getElementById('summary').addEventListener('input', function() {
-    this.value = this.value.replace(/[^\d]/g, '').replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ');
-});
-
-//задаем формат для поле курс, чтобы только цифры были
-document.getElementById('kurs').addEventListener('input', function() {
-    this.value = this.value.replace(/[^0-9.]/g, '', '')
-    // .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$0 ');
-});
-
-
-
 
 tg.MainButton.text = "Готово"; //изменяем текст кнопки 
 tg.MainButton.setText("Готово for Bek"); //изменяем текст кнопки иначе
