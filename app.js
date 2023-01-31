11db78928ffbc4e7be0fbe0f8a52e734f9df3e42
Beknazar19 <45395471+Beknazar19@users.noreply.github.com>
2023-02-01 03:09:20 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -3,34 +3,35 @@
 // tg2.showAlert(`Добро пожаловать, @${WebApp.WebAppUser.username}.`);
 
 
-var val_ask = document.getElementById("select_group_1");
-var val_bid = document.getElementById("select_group_4");
+// var val_ask = document.getElementById("select_group_1");
+// var val_bid = document.getElementById("select_group_4");
 
-function onChange() {
-    var val_ask_num = val_ask.value;
-    var val_ask_text = val_ask.options[val_ask.selectedIndex].text;
+// function onChange() {
+//     var val_ask_num = val_ask.value;
+//     var val_ask_text = val_ask.options[val_ask.selectedIndex].text;
 
-    var val_bid_num = val_bid.value;
-    var val_bid_text = val_bid.options[val_bid.selectedIndex].text;
+//     var val_bid_num = val_bid.value;
+//     var val_bid_text = val_bid.options[val_bid.selectedIndex].text;
 
 
 
-    let searchValue = Math.min(val_ask_num, val_bid_num);
-    let selectElement = document.getElementById('select_group_1');
-    let options = selectElement.options;
+//     let searchValue = Math.min(val_ask_num, val_bid_num);
+//     let selectElement = document.getElementById('select_group_1');
+//     let options = selectElement.options;
 
-    for (let i = 0; i < options.length; i++) {
-        if (options[i].value == searchValue) {
-        console.log(options[i].text, document.getElementById('summary').innerHTML);
+//     for (let i = 0; i < options.length; i++) {
+//         if (options[i].value == searchValue) {
+//         console.log(options[i].text, document.getElementById('summary').innerHTML);
 
-        document.getElementById('Label_Sum').innerHTML = 'Объем, в ' + options[i].text.substring(0, 3);
-        // + options[i].text;
+//         document.getElementById('Label_Sum').innerHTML = 'Объем, в ' + options[i].text.substring(0, 3);
+//         // + options[i].text;
 
-        break;
-        } }
-}
-val_ask.onchange = onChange;
-val_bid.onchange = onChange;
+//         break;
+//         } }
+// }
+
+// val_ask.onchange = onChange;
+// val_bid.onchange = onChange;
 
     // onChange();
 
