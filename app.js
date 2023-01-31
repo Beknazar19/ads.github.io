ff8a819eb2b08a8d2ae7c135d84c5c71ec1d8edc
Beknazar19 <45395471+Beknazar19@users.noreply.github.com>
2023-02-01 03:13:58 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -49,30 +49,30 @@ document.getElementById('kurs').addEventListener('input', function() {
     // .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$0 ');
 });
 
-var value_opt1 =  document.getElementById("select_group_1").value;
-var value_opt2 =  document.getElementById("select_group_2").value;
-var value_opt3 =  document.getElementById("select_group_3").value;
-var value_opt4 =  document.getElementById("select_group_4").value;
-var value_opt5 =  document.getElementById("select_group_5").value;
-var value_opt6 =  document.getElementById("select_group_6").value;
+// var value_opt1 =  document.getElementById("select_group_1").value;
+// var value_opt2 =  document.getElementById("select_group_2").value;
+// var value_opt3 =  document.getElementById("select_group_3").value;
+// var value_opt4 =  document.getElementById("select_group_4").value;
+// var value_opt5 =  document.getElementById("select_group_5").value;
+// var value_opt6 =  document.getElementById("select_group_6").value;
 
-var text_val_ask = value_opt1.options[value_opt1.selectedIndex].text;
-var text_nal_ask = value_opt2.options[value_opt2.selectedIndex].text;
-var text_stamp_ask = value_opt3.options[value_opt3.selectedIndex].text;
-var text_val_bid = value_opt4.options[value_opt4.selectedIndex].text;
-var text_nal_bid = value_opt5.options[value_opt5.selectedIndex].text;
-var text_stamp_bid = value_opt6.options[value_opt6.selectedIndex].text;
+// var text_val_ask = value_opt1.options[value_opt1.selectedIndex].text;
+// var text_nal_ask = value_opt2.options[value_opt2.selectedIndex].text;
+// var text_stamp_ask = value_opt3.options[value_opt3.selectedIndex].text;
+// var text_val_bid = value_opt4.options[value_opt4.selectedIndex].text;
+// var text_nal_bid = value_opt5.options[value_opt5.selectedIndex].text;
+// var text_stamp_bid = value_opt6.options[value_opt6.selectedIndex].text;
 
 
-var dict = {
-    type_web_msg : "from ads",
-    value_opt1 : text_val_ask,
-    value_opt2 : text_nal_ask,
-    value_opt3 : text_stamp_ask,
-    value_opt4 : text_val_bid,
-    value_opt5 : text_nal_bid,
-    value_opt6 : text_stamp_bid
-};
+// var dict = {
+//     type_web_msg : "from ads",
+//     value_opt1 : text_val_ask,
+//     value_opt2 : text_nal_ask,
+//     value_opt3 : text_stamp_ask,
+//     value_opt4 : text_val_bid,
+//     value_opt5 : text_nal_bid,
+//     value_opt6 : text_stamp_bid
+// };
 
 let tg2 = window.Telegram.WebApp;
 
