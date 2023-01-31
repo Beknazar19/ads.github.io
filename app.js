af9fbe4e2727356a15ba142536b7fc22214c0952
Beknazar19 <45395471+Beknazar19@users.noreply.github.com>
2023-02-01 04:37:37 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ var  tg2 = window.Telegram.WebApp;
 tg2.expand(); //расширяем на все окно
 
 tg2.MainButton.text = "Готово"; //изменяем текст кнопки
-tg2.MainButton.setText("Разместить объявление 4"); //изменяем текст кнопки иначе
+tg2.MainButton.setText("Разместить объявление 5"); //изменяем текст кнопки иначе
 tg2.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
 tg2.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
 tg2.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры
